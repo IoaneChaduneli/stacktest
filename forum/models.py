09e06141b347736b645a667f61a6ceadc5a988ac
Ioane <109984297+IoaneChaduneli@users.noreply.github.com>
2023-01-28 11:32:35 +0400

    add migrations

--- forum/models.py
+++ forum/models.py
@@ -1,14 +1,22 @@
 from django.db import models
+from users.models import User
 # Create your models here.
 
 
 class Question(models.Model):
-    pass
-
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=120)
+    text = models.TextField()
+    create_time = models.DateTimeField(auto_now_add=True)
+    modify_time = models. DateTimeField(auto_now=True)
+    views = models.PositiveBigIntegerField(default=0, blank=True)
 
 class Answer(models.Model):
-    pass
-
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.TextField()
+    create_time = models.DateTimeField(auto_now_add=True)
 
 class Tag(models.Model):
-    pass
+    name = models.CharField(max_length=120)
+    question = models.ManyToManyField(Question, blank=True)
--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from forum.models import Question
 # Create your views here.
+
+class HomeView(ListView):
+    model = Question
+    template_name = 'forum/question_list.html'
