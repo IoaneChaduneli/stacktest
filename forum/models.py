9ba4628fdcc95f68a3a19523f756daa87510f6fb
Ioane <109984297+IoaneChaduneli@users.noreply.github.com>
2023-01-28 11:07:51 +0400

    add forum users models

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
 # Create your models here.
+
+
+class Question(models.Model):
+    pass
+
+
+class Answer(models.Model):
+    pass
+
+
+class Tag(models.Model):
+    pass
