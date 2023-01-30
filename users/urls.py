0315fa52af36a5deddfbaca46fc959416775746c
Ioane <109984297+IoaneChaduneli@users.noreply.github.com>
2023-01-30 16:59:59 +0400

    add login-logout

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from users.views import LogoutView
+from users.views import LogoutView, loginView
 app_name = 'users'
 
 urlpatterns = [
-    path('logout/', LogoutView.as_view(), name= 'auth-logout' )
+    path('logout/', LogoutView.as_view(), name= 'auth-logout' ),
+    path('login/', loginView.as_view(), name='auth-login')
 ]
