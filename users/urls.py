22cc7a3f8f50fdb42e90d432d7df9feebd5dd674
Ioane <109984297+IoaneChaduneli@users.noreply.github.com>
2023-01-30 20:12:14 +0400

    add view update

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from users.views import LogoutView, loginView
+from users.views import LogoutView, loginView, RegisterView
 app_name = 'users'
 
 urlpatterns = [
     path('logout/', LogoutView.as_view(), name= 'auth-logout' ),
-    path('login/', loginView.as_view(), name='auth-login')
+    path('login/', loginView.as_view(), name='auth-login'),
+    path('signup/', RegisterView.as_view(), name='auth-signup')
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-
+from django.views.generic import CreateView
 from django.contrib.auth.views import LogoutView as DjangoLogoutView, LoginView as DjangoLoginView
+from users.models import User
+from users.forms import UserCreationForm
 # Create your views here.
 
 class LogoutView(DjangoLogoutView):
@@ -11,3 +13,9 @@ class loginView(DjangoLoginView):
     next_page = reverse_lazy('forum:home')
     template_name = 'auth/login.html'
     redirect_authenticated_user = False
+
+class RegisterView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    template_name = 'auth/login.html'
+    success_url = reverse_lazy('forum:home')
